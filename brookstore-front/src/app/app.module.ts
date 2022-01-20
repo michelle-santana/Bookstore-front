38cf13c1a09bb1e37e0afe76916b048e4bbe3067
michelle-santana <michelle.raiadrogasil@gmail.com>
2022-01-20 17:37:42 -0300

    component delete começando

--- brookstore-front/src/app/app.module.ts
+++ brookstore-front/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
-    CategoriaCreateComponent
+    CategoriaCreateComponent,
+    CategoriaDeleteComponent
   ],
   imports: [
     BrowserModule,
