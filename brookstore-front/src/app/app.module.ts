36f1eef778529233ca775d39e290243bceb8c706
michelle-santana <michelle.raiadrogasil@gmail.com>
2022-01-20 21:34:37 -0300

    componente livro all

--- brookstore-front/src/app/app.module.ts
+++ brookstore-front/src/app/app.module.ts
@@ -23,6 +23,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
     CategoriaReadComponent,
     CategoriaCreateComponent,
     CategoriaDeleteComponent,
-    CategoriaUpdateComponent
+    CategoriaUpdateComponent,
+    LivroReadAllComponent,
+    
   ],
   imports: [
     BrowserModule,
